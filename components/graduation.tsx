36340ae39ec38dac77f01505667c540b02b70f85
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2026-05-13 00:06:37 +0530

    fixing comp and added images

--- components/graduation.tsx
+++ components/graduation.tsx
@@ -1,6 +1,6 @@
 export default function(){
     return <div className="bg-linear-to-r from-[#007EFF] to-[#0449CE] py-2">
-                <div className="r-w space-y-2">
+                <div className="mx-4 r-w space-y-2">
                     <h2 className="text-xl md:text-3xl text-center text-white font-black">Graduation Ceremony 2025 - Dubai</h2>
                     <p className="text-white text-center text-sm md:w-3xl md:mx-auto">International Schooling hosted its first 
                         Graduation Ceremony in 2025 at Atlantis, The Palm, Dubai, reflecting our global community and commitment 
