fc62c72133e99f080f792c00d3580957359f3931
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2026-05-19 15:06:07 +0530

    added dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,41 @@
+import dynamic from "next/dynamic";
+
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
 import Stats from "@/components/stats";
-import Trust from "@/components/trust";
-// import Features from "@/components/features";
-import Testimonials from "@/components/testimonials";
-// import FAQ from "@/components/faq";
-import Footer from "@/components/footer";
 import Graduation from "@/components/graduation";
-import Learningplan from "@/components/learningplan";
-import Whychooseus from "@/components/whychooseus";
-import Ratings from "@/components/ratings";
-import Paymentsupport from "@/components/paymentsupport";
-import Accredition from "@/components/accredition";
-import Accordian from "@/components/accordian";
+
+const Trust = dynamic(() => import("@/components/trust"));
+const Learningplan = dynamic(() => import("@/components/learningplan"));
+const Whychooseus = dynamic(() => import("@/components/whychooseus"));
+const Testimonials = dynamic(() => import("@/components/testimonials"));
+const Ratings = dynamic(() => import("@/components/ratings"));
+const Paymentsupport = dynamic(() => import("@/components/paymentsupport"));
+const Accredition = dynamic(() => import("@/components/accredition"));
+const Accordian = dynamic(() => import("@/components/accordian"));
+const Footer = dynamic(() => import("@/components/footer"));
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <div className="bg-[#46dd6b] text-white"><Hero/></div>
-      <Stats/>
-      <Graduation/>
-      <Trust/>
-      <Learningplan/>
-      <Whychooseus/>
-      <Testimonials/>
-      <Ratings/>
-      <Paymentsupport/>
-      <Accredition/>
-      <Accordian/>
-      <Footer/>
+
+      <div className="bg-[#46dd6b] text-white">
+        <Hero />
+      </div>
+
+      <Stats />
+      <Graduation />
+
+       <Trust />
+      <Learningplan />
+      <Whychooseus />
+      <Testimonials />
+      <Ratings />
+      <Paymentsupport />
+      <Accredition />
+      <Accordian />
+      <Footer />
     </main>
   );
 }
