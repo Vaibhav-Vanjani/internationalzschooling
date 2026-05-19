c152e90a84403860f3f781f8614e329ad1b7a35c
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2026-05-19 16:05:01 +0530

    minor fix

--- app/uae/page.tsx
+++ app/uae/page.tsx
@@ -1,7 +1,9 @@
+import dynamic from "next/dynamic";
+
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+
 import Trust from "@/components/country/uae/trust";
-import Parentreview from "@/components/country/uae/parentreview";
 import Recommended from "@/components/country/uae/recommended";
 import Accredation from "@/components/country/uae/accredation";
 import Accredationcards from "@/components/country/uae/accredationcards";
@@ -9,38 +11,78 @@ import WhyFamiliesChoose from "@/components/country/uae/whychooseus";
 import Learningplan from "@/components/learningplan";
 import CurriculumSection from "@/components/country/uae/curicullam";
 import Curicullamcards from "@/components/country/uae/curicullamcards";
-import { elementarySchoolData,highSchoolData,middleSchoolData } from "@/components/constants/coursecarddata";
-import { enrollmentSteps } from "@/components/constants/enrollmentstep";
 import BenefitsSection from "@/components/country/uae/benifits";
 import Feestructure from "@/components/country/uae/feestructure";
 import EnrollmentSteps from "@/components/country/uae/enrollnow";
-import BlogGrid from "@/components/country/uae/blog";
-import FAQs from "@/components/country/uae/faq";
 import EnrollCTA from "@/components/country/uae/enrollcta";
 
-export default function() {
-    const curicullamCardList = [elementarySchoolData,middleSchoolData,highSchoolData];
+import {
+  elementarySchoolData,
+  highSchoolData,
+  middleSchoolData,
+} from "@/components/constants/coursecarddata";
+
+import { enrollmentSteps } from "@/components/constants/enrollmentstep";
+
+/* LAZY LOAD BELOW-THE-FOLD COMPONENTS */
+const Parentreview = dynamic(
+  () => import("@/components/country/uae/parentreview")
+);
+
+const BlogGrid = dynamic(
+  () => import("@/components/country/uae/blog")
+);
+
+const FAQs = dynamic(
+  () => import("@/components/country/uae/faq")
+);
+
+export default function UAEPage() {
+  const curriculumCardList = [
+    elementarySchoolData,
+    middleSchoolData,
+    highSchoolData,
+  ];
+
   return (
     <main className="bg-[#F7FBFF]">
+      {/* ABOVE THE FOLD */}
       <Navbar />
-      <Trust/>
-      <Parentreview/>
-      <Recommended/>
-      <Accredation/>
-      <Accredationcards/>
-      <WhyFamiliesChoose/>
-      <Learningplan/>
-      <CurriculumSection/>
-      {curicullamCardList.map((item,index)=>{
-        return <Curicullamcards key={index} title={item.title} groupLearning={item.groupLearning} oneToOne={item.oneToOne}/>
-      })}
-      <BenefitsSection/>
-      <Feestructure/>
+      <Trust />
+
+      {/* IMPORTANT CONTENT */}
+      <Recommended />
+      <Accredation />
+      <Accredationcards />
+      <WhyFamiliesChoose />
+      <Learningplan />
+      <CurriculumSection />
+
+      {/* CURRICULUM CARDS */}
+      {curriculumCardList.map((item) => (
+        <Curicullamcards
+          key={item.title}
+          title={item.title}
+          groupLearning={item.groupLearning}
+          oneToOne={item.oneToOne}
+        />
+      ))}
+
+      {/* LOWER PRIORITY */}
+      <BenefitsSection />
+      <Feestructure />
       <EnrollmentSteps {...enrollmentSteps} />
-      <BlogGrid/>
-      <FAQs/>
-      <EnrollCTA/>
-      <Footer/>
+
+      {/* BELOW THE FOLD */}
+      <Parentreview />
+      <BlogGrid />
+      <FAQs />
+
+      {/* CTA */}
+      <EnrollCTA />
+
+      {/* FOOTER */}
+      <Footer />
     </main>
   );
 }
